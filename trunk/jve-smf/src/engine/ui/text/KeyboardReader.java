1db6a661f9b681e3943ed1abdeffffbc82985d08
thiagonobrega <thiagonobrega@30404827-9f25-0410-8afb-6b50d5c0048e>
2007-06-03 13:53:51 +0000

    restaurando para uma verção que funcione ( eu quebrei =) )

---
+++ trunk/jve-smf/src/engine/ui/text/KeyboardReader.java
@@ -0,0 +1,341 @@
+/*
+ * JGVE - J Grid Video Editor.
+ *
+ * Copyright (c) 2007, Thiago Nóbrega
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 2
+ * of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ *
+ * KeyboardReader.java created in 30/01/2007 - 7:53:27 PM
+ * by Thiago Nobrega ( thiagonobrega at gmail dot com )
+ */
+package engine.ui.text;
+
+import image.filters.statics.pontuals.color.Color2GrayScale;
+import image.filters.statics.pontuals.logical.IGSFilter;
+
+import java.awt.Color;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+import javax.media.Codec;
+import javax.media.MediaLocator;
+import javax.media.NoDataSinkException;
+import javax.media.NoProcessorException;
+import javax.media.NotConfiguredError;
+import javax.media.NotRealizedError;
+import javax.media.UnsupportedPlugInException;
+
+import video.effects.VideoEffect;
+import video.effects.motion.MotionDetectEffect;
+import video.effects.statics.SimpleEffect;
+import video.filters.motion.BorderMotionDetectionFilter;
+import video.util.VideoWorker;
+import engine.ui.UIException;
+import engine.util.Messages;
+import engine.util.MessagesErros;
+
+public class KeyboardReader implements Runnable {
+
+	private Scanner in;
+
+	private String[] instructions;
+
+	private List<String> userFiltersName;
+
+	private Map<String, VideoEffect> userFilters;
+
+	private String[] filterList;
+
+	private String[] colors;
+
+	public KeyboardReader() {
+
+		in = new Scanner(new InputStreamReader(System.in));
+
+		in.useDelimiter("\n");
+
+		instructions = new String[] { Messages.getString("command.exit"),
+				Messages.getString("command.help"),
+				Messages.getString("command.new"),
+				Messages.getString("command.apply"),
+				Messages.getString("command.list.user.filters") };
+
+		userFilters = new HashMap<String, VideoEffect>();
+		userFiltersName = new LinkedList<String>();
+
+		filterList = new String[] { Messages.getString("filter.motion.detect") ,
+				Messages.getString("filter.static.igs") ,
+				Messages.getString("filter.static.color.color2gray")};
+
+		colors = new String[] { Messages.getString("color.red"),
+				Messages.getString("color.green"),
+				Messages.getString("color.blue"),
+				Messages.getString("color.black"),
+				Messages.getString("color.yellow"),
+				Messages.getString("color.cyan"), };
+	}
+
+	public void run() {
+
+		while (true) {
+			try {
+				while (in.hasNext()) {
+
+					String line = in.next().trim();
+
+					StringTokenizer word = new StringTokenizer(line, " ");
+
+					// verify if has a valid entry
+					if (word.countTokens() <= 0)
+						throw new UIException(MessagesErros
+								.getString("error.inavlid.arguments.numbers"));
+
+					// get the instruction
+					int instruction = getInstruction(instructions, word
+							.nextToken());
+
+					switch (instruction) {
+
+					case 0:
+						System.out.println(Messages.getString("sucess.exit"));
+						System.exit(0);
+						break;
+					case 1:
+						System.out.println(Messages
+								.getString("help.textui.usage"));
+						break;
+					case 2: // new insttuction.
+						createNewFilter(word);
+						break;
+					case 3: // apply
+						applyFilters(word);
+						break;
+					case 4: // list
+						listUserFilters();
+						break;
+					default:
+						System.out.println("?");
+						break;
+					}
+
+				}
+
+			} catch (Exception e) {
+				System.err.println(e.getMessage());
+			}
+
+		}// while true
+
+	}
+
+	private void listUserFilters() {
+		System.out.print("[ ");
+		for (String s : userFiltersName) {
+			System.out.print(s + " ");
+		}
+		System.out.println("]");
+	}
+
+	private void applyFilters(StringTokenizer word) throws UIException,
+			NoProcessorException, UnsupportedPlugInException,
+			NotConfiguredError, NotRealizedError, NoDataSinkException,
+			SecurityException, IOException {
+		List<VideoEffect> tempEfxList = new LinkedList<VideoEffect>();
+		File inputFile = null;
+		String input = null;
+		String output = null;
+		String n = null;
+
+		boolean outFlag = false;
+		while (!outFlag) {
+
+			n = word.nextToken();
+
+			if (n.equalsIgnoreCase(Messages.getString("command.apply.filter"))
+					|| n == null) {
+
+				n = word.nextToken();
+
+				VideoEffect efx = userFilters.get(n);
+
+				if (efx == null)
+					throw new UIException(MessagesErros
+							.getString("error.filter.cantfind")
+							+ n);
+
+				tempEfxList.add(efx);
+			} else
+				outFlag = true;
+
+		}
+
+		if (n.equalsIgnoreCase(Messages.getString("command.input"))) {
+			input = word.nextToken();
+			inputFile = new File(input);
+		}
+
+		if (word.nextToken().equalsIgnoreCase(
+				Messages.getString("command.output")))
+			output = word.nextToken();
+
+		// verificacoes
+
+		if (!inputFile.exists())
+			throw new UIException(MessagesErros
+					.getString("error.file.cantfind.input")
+					+ input + ".");
+		if (output == null)
+			throw new UIException(MessagesErros
+					.getString("error.file.exist.output"));
+
+		String filePrefix = "file:/";
+
+		VideoWorker fa = new VideoWorker(new MediaLocator(filePrefix + input),
+				new MediaLocator(filePrefix + output));
+
+		Codec[] cs = new Codec[tempEfxList.size()];
+
+		int i = 0;
+		for (Codec c : tempEfxList) {
+			cs[i] = c;
+		}
+
+		fa.setCodec(cs);
+		// FIXME inciar um thread separada
+
+		fa.open();
+	}
+
+	private void createNewFilter(StringTokenizer word) throws UIException {
+
+		if (!word.nextToken().equalsIgnoreCase(
+				Messages.getString("command.new.filter")))
+			throw new UIException(MessagesErros
+					.getString("error.invalid.object"));
+
+		// get the filter
+		int filterNum = getInstruction(filterList, word.nextToken());
+
+		String filterAlias = word.nextToken();
+
+		switch (filterNum) {
+		case 0: // motion detection
+			newMotionFilter(word, filterAlias);
+			break;
+		case 1: // grayscale
+			SimpleEffect thisEfx = new SimpleEffect(new IGSFilter());
+			userFilters.put(filterAlias, thisEfx);
+			userFiltersName.add(filterAlias);
+			System.out.println(Messages.getString("sucess.textui.filters.create"));
+			break;
+		case 2: //igs
+			SimpleEffect thisEfxc = new SimpleEffect(new Color2GrayScale());
+			userFilters.put(filterAlias, thisEfxc);
+			userFiltersName.add(filterAlias);
+			System.out.println(Messages.getString("sucess.textui.filters.create"));
+			break;
+		default:
+			System.out.println("---x---");
+			break;
+		}
+
+	}
+
+	/**
+	 * create a new motion filter
+	 *
+	 * @param word
+	 * @param filterAlias
+	 * @throws UIException
+	 */
+	private void newMotionFilter(StringTokenizer word, String filterAlias)
+			throws UIException {
+		Color color = null;
+		if (word.hasMoreTokens()) {
+			String n = word.nextToken();
+
+			if (n.equalsIgnoreCase("help")) {
+				System.out.println(Messages
+						.getString("help.textui.motion.detect"));
+				return;
+			}
+
+			if (n.startsWith("-c")) {
+				if (!word.hasMoreTokens())
+					new UIException(MessagesErros.getString("error.color.null"));
+
+				int colorNum = getInstruction(colors, word.nextToken());
+
+				switch (colorNum) {
+
+				case 0:
+					color = Color.red;
+					break;
+				case 1:
+					color = Color.green;
+					break;
+				case 2:
+					color = Color.blue;
+					break;
+				case 3:
+					color = Color.black;
+					break;
+				case 4:
+					color = Color.yellow;
+					break;
+				case 5:
+					color = Color.cyan;
+					break;
+				default:
+					throw new UIException(Messages
+							.getString("error.color.invalid"));
+				} // switch color
+			} // if from color
+		} // if hasmoretokens
+
+		BorderMotionDetectionFilter thisFilter = color == null ? new BorderMotionDetectionFilter()
+				: new BorderMotionDetectionFilter(color);
+
+		userFilters.put(filterAlias, new MotionDetectEffect(thisFilter));
+		userFiltersName.add(filterAlias);
+
+		System.out.println(Messages.getString("sucess.textui.filters.create"));
+	}
+
+	private int getInstruction(String[] instructionset, String input) {
+
+		for (int i = 0; i < instructionset.length; i++) {
+			if (input.equalsIgnoreCase(instructionset[i]))
+				return i;
+		}
+		return -1;
+	}
+
+	public static void main(String[] args) {
+
+		KeyboardReader kbr = new KeyboardReader();
+		kbr.run();
+
+	}
+
+}
---
+++ trunk/jve-smf/src/image/filters/statics/pontuals/aritimetics/MINFilter.java
@@ -0,0 +1,112 @@
+package image.filters.statics.pontuals.aritimetics;
+
+import image.filters.PointFilter;
+
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+
+public class MINFilter implements PointFilter {
+
+	private BufferedImage image1;
+	private BufferedImage image2;
+	private int constant;
+
+	private static String name = "Filtro Multiplicativo";
+	private static String group = "Pontual Aritim�tico";
+
+	public MINFilter (BufferedImage i, int RGB) {
+		image1 = i;
+		constant = RGB;
+	}
+
+	public MINFilter (BufferedImage i1, BufferedImage i2, int x, int y) {
+		image1 = i1;
+		image2 = i2;
+		constant = 255;
+	}
+
+
+	public BufferedImage process() {
+		int linesImage1 = image1.getHeight();
+		int columImage1 = image1.getWidth();
+		int linesImage2 = 0;
+		int columsIMage2 = 0;
+
+		if ( image2 != null ){
+			linesImage2 = image2.getHeight();
+			columsIMage2 = image2.getWidth();
+		}
+
+		int coluna = Math.max(columImage1,columsIMage2);
+		int linha = Math.max(linesImage1,linesImage2);
+
+		System.out.println("C "+ coluna+"\nL "+linha+"\n");
+
+		BufferedImage saida = new BufferedImage(coluna,linha,BufferedImage.TYPE_INT_RGB);
+
+		Color color1;
+		int b1,b2;
+		int r1,r2;
+		int g1,g2;
+
+		for ( int c = 0 ; c < coluna ; c++){
+			for ( int l = 0 ; l < linha ; l++ ){
+
+
+				if ( l < linesImage1 && c < columImage1 ){
+					color1 =  new Color(image1.getRGB(c,l));
+					b1 = color1.getBlue();
+					r1 = color1.getRed();
+					g1 = color1.getGreen();
+				} else{
+					b1 = 0;
+					r1 = 0;
+					g1 = 0;
+				}
+
+				if ( (l < linesImage2  &&  c < columsIMage2) && image2 != null ) {
+					color1 =  new Color(image2.getRGB(c,l));
+					b2 = color1.getBlue();
+					r2 = color1.getRed();
+					g2 = color1.getGreen();
+				} else{
+					b2 = 0;
+					r2 = 0;
+					g2 = 0;
+				}
+
+
+				saida.setRGB(c,l, new Color(Math.min(Math.min(r1,r2),constant),Math.min(Math.min(g1,g2),constant),Math.min(Math.min(g1,g2),constant)).getRGB());
+
+			}// linhas
+		}//colunas
+		return saida;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getGroup() {
+		return group;
+	}
+
+	public void setConstant(int i) {
+		constant = i;
+
+	}
+
+	public int getNullConstant() {
+		return 255;
+	}
+
+	public void setSecondImage(BufferedImage second) {
+		this.image2 = second;
+	}
+
+	public void setWorkingImage(BufferedImage wimg) {
+		image1 = wimg;
+
+	}
+
+}
