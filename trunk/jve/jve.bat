e739b05accb5b4d1c5d7a9275e414443d98f8e00
thiagonobrega <thiagonobrega@30404827-9f25-0410-8afb-6b50d5c0048e>
2007-06-03 15:25:02 +0000

    rename packages to jve.*

--- trunk/jve/jve.bat
+++ trunk/jve/jve.bat
@@ -1,2 +1,2 @@
 cls
-java -Dcom.sun.management.jmxremote -classpath lib\log4j-1.2.14.jar;lib\jmf.jar;lib\fobs4jmf.jar;bin\ engine.ui.text.KeyboardReader
+java -Dcom.sun.management.jmxremote -classpath lib\log4j-1.2.14.jar;lib\jmf.jar;lib\fobs4jmf.jar;bin\ jve.engine.ui.text.KeyboardReader
